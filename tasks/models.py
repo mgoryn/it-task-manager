7d6cc9487241c419e98c4495c0185a0b5ae05088
Misha Goryn <mgoryn68@gmail.com>
2024-10-08 20:19:01 +0300

    add new models: position, worker, tasktype and task & set custom user model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Medium", "Medium"),
+        ("Low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_complete = models.BooleanField(default=False)
+    priority = models.CharField(max_length=255, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True, blank=True)
+    assignees = models.ManyToManyField(Worker, related_name="tasks")
+
+    def __str__(self):
+        return self.name
