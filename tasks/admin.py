0726955ca32f5cef3b304b3cadb9f1b8c85b0359
Misha Goryn <mgoryn68@gmail.com>
2024-10-15 22:02:18 +0300

    Configure admin panel for Task Manager

--- tasks/admin.py
+++ tasks/admin.py
@@ -30,3 +30,13 @@ class ProjectAdmin(admin.ModelAdmin):
 class TeamAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
