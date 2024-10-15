8db6a82cd2e4bf5f80c8a6cbb1de38e63b49fd57
Misha Goryn <mgoryn68@gmail.com>
2024-10-15 22:01:10 +0300

    Configure admin panel for Task Manager

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from tasks.models import Task, Worker, TaskType, Project, Team, Position
 
-# Register your models here.
+
+@admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "worker", "task_type", "project", "created_at", "updated_at")
+    search_fields = ("name",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = ("user", "position")
+    search_fields = ("user__username",)
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at",)
+    search_fields = ("name",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_at",)
+    search_fields = ("name",)
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
