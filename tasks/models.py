ffe6384ef790adb5a4a596435988d5e9752f1072
Misha Goryn <mgoryn68@gmail.com>
2024-10-15 20:48:23 +0300

    add models for Task, Project, Position, Worker, TaskType, Team

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    worker = models.ForeignKey(Worker, on_delete=models.SET_NULL, null=True)
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    members = models.ManyToManyField(Worker)
+
+    def __str__(self):
+        return self.name
+
