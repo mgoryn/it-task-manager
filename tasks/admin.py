44fa5007ad8a8670a28130010dc9c74d50afeabe
Misha Goryn <mgoryn68@gmail.com>
2024-10-21 21:56:40 +0300

    Added Task, TaskType, Position, Project, Tag, Worker admins models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from tasks.models import Task, TaskType, Position, Project, Tag, Worker
 
-# Register your models here.
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    search_fields = ("name", "description")
+    filter_horizontal = ("team_members",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "deadline", "is_completed", "priority")
+    search_fields = ("name", "description")
+    list_filter = ("priority", "is_complete", "project")
+    filter_horizontal = ("assignees", "tags")
+
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name",)
